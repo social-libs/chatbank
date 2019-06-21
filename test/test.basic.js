d54eb6ac1eba3a55113f0edb81e8ab0731f93e6e
Andrija Petrovic <andrija.hers@gmail.com>
2019-06-21 08:57:35 -0400

    + a fix in the basic test

--- test/test.basic.js
+++ test/test.basic.js
@@ -1,18 +1,10 @@
-var lib = require('./lib');
-function conversationNotificationHandler (convntf) {
-  setGlobal('LastConversation', convntf);
-}
+var testlib = require('./lib');
 describe('Basic Test', function () {
   it('Load Library', function () {
     return setGlobal('Lib', require('../')(execlib));
   });
   it('Create Bank', function () {
-    return setGlobal('Bank', lib.createBank('basictest.db', true));
-  });
-  it('Listen to Bank conversation notifications', function () {
-    Bank.conversationsDefer.promise.then(
-      null, null, conversationNotificationHandler
-    );
+    return setGlobal('Bank', testlib.createBank('basictest.db', true));
   });
   it('Process new P2P Message', function () {
     return qlib.promise2console(Bank.processNewMessage('andra', null, 'luka', 'blah'), 'P2P');
@@ -24,18 +16,18 @@ describe('Basic Test', function () {
     return qlib.promise2console(Bank.createNewGroup('luka'), 'New Group');
   });
   it('Creator sends a message to 1-user group', function () {
-    return qlib.promise2console(Bank.processNewMessage('luka', LastConversation.id, null, 'There you go!'), 'Creator send a message');
+    return qlib.promise2console(Bank.processNewMessage('luka', LastConversationNotified.id, null, 'There you go!'), 'Creator send a message');
   });
   it('Add a new user to Group', function () {
-    return qlib.promise2console(Bank.addUserToGroup(LastConversation.id, 'luka', 'ra'), 'Creator adds a User');
+    return qlib.promise2console(Bank.addUserToGroup(LastConversationNotified.id, 'luka', 'ra'), 'Creator adds a User');
   });
   it('Newly added user sends a message to 1-user group', function () {
-    return qlib.promise2console(Bank.processNewMessage('luka', LastConversation.id, null, 'There you go!'), 'New User sends a message');
+    return qlib.promise2console(Bank.processNewMessage('luka', LastConversationNotified.id, null, 'There you go!'), 'New User sends a message');
   });
   it('Newly added user adds another user to Group', function () {
-    return qlib.promise2console(Bank.addUserToGroup(LastConversation.id, 'ra', 'andra'), 'New User adds another User to Group');
+    return qlib.promise2console(Bank.addUserToGroup(LastConversationNotified.id, 'ra', 'andra'), 'New User adds another User to Group');
   });
   it('Latest user sends a message to Group', function () {
-    return qlib.promise2console(Bank.addUserToGroup(LastConversation.id, 'luka', 'ra'), 'Last users sends a message');
+    return qlib.promise2console(Bank.addUserToGroup(LastConversationNotified.id, 'luka', 'ra'), 'Last users sends a message');
   });
 });
